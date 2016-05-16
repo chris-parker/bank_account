b771ffd76dde02414b3842168b12ec1ab7c4ed06
Chris Parker <cjp214@gmail.com>
2016-05-16 10:57:08 +0100

    added deposit and withdrawal methods, all tests passing

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,4 +2,16 @@ class Bank_account
 
   attr_reader :balance
 
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdrawal(amount)
+    @balance -= amount
+  end
+
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,9 +2,30 @@ require 'bank_account'
 
 describe 'bank_account' do
 
-  it 'initializes with a balance' do
-    bank_account = Bank_account.new
-    expect(bank_account.balance).to be(nil)
+  context 'balance' do
+    it 'initializes with a balance' do
+      bank_account = Bank_account.new
+      expect(bank_account.balance).to eq(0)
+    end
   end
+
+  context 'deposit' do
+    it 'adds to the balance' do
+      bank_account = Bank_account.new
+      bank_account.deposit(500)
+      expect(bank_account.balance).to eq(500)
+    end
+  end
+
+  context 'withdrawal' do
+    it 'takes away from the balance' do
+      bank_account = Bank_account.new
+      bank_account.deposit(500)
+      bank_account.withdrawal(300)
+      expect(bank_account.balance).to eq(200)
+    end
+  end
+
+
 
 end
