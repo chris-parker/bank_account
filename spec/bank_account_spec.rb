d93e551ac2219743242f14c079ebbb5d365c3d62
Chris Parker <cjp214@gmail.com>
2016-05-16 14:47:39 +0100

    refactored tests slightly with before(:each) helpers

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,43 +3,48 @@ require 'bank_account'
 describe Bank_account do
   subject(:bank_account) { described_class.new }
 
-  context 'balance' do
+  context 'initialize' do
     it 'initializes with a balance' do
       expect(bank_account.balance).to eq(0)
+    end
+
+    it 'initializes with a blank statement' do
+      expect(bank_account.print_statement).to eq("date || credit || debit || balance\n")
     end
   end
 
   context 'deposit' do
-    it 'adds to the balance' do
+
+    before(:each) do
       bank_account.deposit(1000, "10/01/2012")
+    end
+
+    it 'adds to the balance' do
       expect(bank_account.balance).to eq(1000.00)
     end
 
     it 'adds a line to the statement' do
-      bank_account.deposit(1000, "10/01/2012")
       expect(bank_account.transactions.last).to eq("10/01/2012 || 1000.00 || || 1000.00")
     end
   end
 
   context 'withdraw' do
-    it 'takes away from the balance' do
+
+    before(:each) do
       bank_account.deposit(1000, "10/01/2012")
       bank_account.withdraw(500, "14/01/2012")
+    end
+
+    it 'takes away from the balance' do
       expect(bank_account.balance).to eq(500.00)
     end
 
     it 'adds a line to the statement' do
-      bank_account.deposit(1000, "10/01/2012")
-      bank_account.withdraw(500, "14/01/2012")
       expect(bank_account.transactions.last).to eq("14/01/2012 || || 500.00 || 500.00")
     end
   end
 
   context 'print_statement' do
-    it 'initializes with a blank statement' do
-      expect(bank_account.print_statement).to eq("date || credit || debit || balance\n")
-    end
-
     it 'prints a transaction history' do
       bank_account.deposit(1000, "10/01/2012")
       bank_account.deposit(2000, "13/01/2012")
