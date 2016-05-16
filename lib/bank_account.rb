9206f5846c33135975b8a03a9b0ce92858d9ddbd
Chris Parker <cjp214@gmail.com>
2016-05-16 12:16:33 +0100

    refactored print_statement, removed statement_header

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,33 +1,34 @@
 class Bank_account
 
-  attr_reader :balance, :statement_header, :statement
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
-    @statement_header = "date || credit || debit || balance"
-    @statement = []
+    @transactions = []
   end
 
   def deposit(amount, date)
     @balance += amount
-    add_deposit_to_statement(amount, date)
+    add_deposit_to_transactions(amount, date)
   end
 
   def withdrawal(amount, date)
     @balance -= amount
-    add_withdrawal_to_statement(amount, date)
-  end
-
-  def add_deposit_to_statement(amount, date)
-    @statement << date + " || " + amount.to_s + "0 || || #{@balance}0"
-  end
-
-  def add_withdrawal_to_statement(amount, date)
-    @statement << date + " || || " + amount.to_s + "0 || #{@balance}0"
+    add_withdrawal_to_transactions(amount, date)
   end
 
   def print_statement
-    @statement_header + "\n" + @statement.reverse.join("\n")
+    "date || credit || debit || balance\n" + @transactions.reverse.join("\n")
+  end
+
+  private
+
+  def add_deposit_to_transactions(amount, date)
+    @transactions << date + " || " + amount.to_s + "0 || || #{@balance}0"
+  end
+
+  def add_withdrawal_to_transactions(amount, date)
+    @transactions << date + " || || " + amount.to_s + "0 || #{@balance}0"
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,52 +1,46 @@
 require 'bank_account'
 
-describe 'bank_account' do
+describe Bank_account do
+  subject(:bank_account) { described_class.new }
 
   context 'balance' do
     it 'initializes with a balance' do
-      bank_account = Bank_account.new
       expect(bank_account.balance).to eq(0)
     end
   end
 
   context 'deposit' do
     it 'adds to the balance' do
-      bank_account = Bank_account.new
-      bank_account.deposit(500.00, "16/05/2016")
-      expect(bank_account.balance).to eq(500.00)
+      bank_account.deposit(1000.00, "10/01/2012")
+      expect(bank_account.balance).to eq(1000.00)
     end
 
     it 'adds a line to the statement' do
-      bank_account = Bank_account.new
-      bank_account.deposit(500.00, "16/05/2016")
-      expect(bank_account.statement.last).to eq("16/05/2016 || 500.00 || || 500.00")
+      bank_account.deposit(1000.00, "10/01/2012")
+      expect(bank_account.transactions.last).to eq("10/01/2012 || 1000.00 || || 1000.00")
     end
   end
 
   context 'withdrawal' do
     it 'takes away from the balance' do
-      bank_account = Bank_account.new
-      bank_account.deposit(500.00, "16/05/2016")
-      bank_account.withdrawal(300.00, "16/05/2016")
-      expect(bank_account.balance).to eq(200.00)
+      bank_account.deposit(1000.00, "10/01/2012")
+      bank_account.withdrawal(500.00, "14/01/2012")
+      expect(bank_account.balance).to eq(500.00)
     end
 
     it 'adds a line to the statement' do
-      bank_account = Bank_account.new
-      bank_account.deposit(500.00, "16/05/2016")
-      bank_account.withdrawal(300.00, "16/05/2016")
-      expect(bank_account.statement.last).to eq("16/05/2016 || || 300.00 || 200.00")
+      bank_account.deposit(1000.00, "10/01/2012")
+      bank_account.withdrawal(500.00, "14/01/2012")
+      expect(bank_account.transactions.last).to eq("14/01/2012 || || 500.00 || 500.00")
     end
   end
 
-  context 'statement' do
+  context 'print_statement' do
     it 'initializes with a blank statement' do
-      bank_account = Bank_account.new
-      expect(bank_account.statement_header).to eq("date || credit || debit || balance")
+      expect(bank_account.print_statement).to eq("date || credit || debit || balance\n")
     end
 
     it 'prints a transaction history' do
-      bank_account = Bank_account.new
       bank_account.deposit(1000.00, "10/01/2012")
       bank_account.deposit(2000.00, "13/01/2012")
       bank_account.withdrawal(500.00, "14/01/2012")
